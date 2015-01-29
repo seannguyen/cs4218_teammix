6da77635c860f3df76b6fd4986986a5659078c96
melvinlzw <melvinlzw@gmail.com>
2015-01-30 00:00:26 +0800

    remove abstract application class. CD class is done. Yet to be tested

--- src/sg/edu/nus/comp/cs4218/impl/app/Cd.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/Cd.java
@@ -1,31 +1,22 @@
 package sg.edu.nus.comp.cs4218.impl.app;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Paths;
 
 import sg.edu.nus.comp.cs4218.Application;
-import sg.edu.nus.comp.cs4218.AbstractApplication;
+import sg.edu.nus.comp.cs4218.Environment;
 import sg.edu.nus.comp.cs4218.exception.CdException;
 import sg.edu.nus.comp.cs4218.util.common_util;
 
-public class Cd extends AbstractApplication implements Application{
-  private File workingDirectory;
-  
-  public Cd(String[] arguments) {
-    super(arguments);
-    if(args == null || args.length == 0 || !args[0].equals("cd")) {
-      //set status code
-    }
-  }
+public class Cd implements Application{
+  private Environment environment;
   
   public File changeDirectory(String newDirectory) throws CdException {
       if (newDirectory != null){
           File newDir = new File(newDirectory);
           if (newDir.isDirectory()){
-              //setStatusCode(55);
               return newDir;
           }
       } else {
@@ -45,21 +36,14 @@ public class Cd extends AbstractApplication implements Application{
         }else if (Paths.get(args[1]).isAbsolute()){
           newDirectory = changeDirectory(args[1]);
         }else{
-          newDirectory = changeDirectory(common_util.formatDirectory(workingDirectory.getAbsolutePath(), args[1]));
+          newDirectory = changeDirectory(common_util.formatDirectory(environment.currentDirectory, args[1]));
         }
     }else{
       throw new CdException("Invalid arguments");
     }
     
     if (newDirectory != null){
-        workingDirectory =  new File(newDirectory.getAbsolutePath());
+        environment.currentDirectory =  newDirectory.getAbsolutePath();
     }
-    
-    try {
-      stdout.write(workingDirectory.toString().getBytes());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    
   }
 }
--- src/sg/edu/nus/comp/cs4218/impl/app/Echo.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/Echo.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.nio.file.Paths;
 
 import sg.edu.nus.comp.cs4218.Application;
-import sg.edu.nus.comp.cs4218.AbstractApplication;
 
 public class Echo {
 
