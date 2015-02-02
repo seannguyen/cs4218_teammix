f89c5feabfb107d1651c0810841446641ecaacc8
weijian3030 <weijian3030@gmail.com>
2015-02-03 01:16:34 +0800

    added cat, head and tail

--- src/sg/edu/nus/comp/cs4218/shell/SimpleShell.java
+++ src/sg/edu/nus/comp/cs4218/shell/SimpleShell.java
@@ -31,6 +31,9 @@ public class SimpleShell implements Shell {
 		Environment.nameAppMaps.put(Configurations.APPNAME_CD, new Cd(Environment.currentDirectory));
 		Environment.nameAppMaps.put(Configurations.APPNAME_LS, new Ls(Environment.currentDirectory));
 		Environment.nameAppMaps.put(Configurations.APPNAME_ECHO, new Echo());
+		Environment.nameAppMaps.put(Configurations.APPNAME_CAT, new Cat());
+		Environment.nameAppMaps.put(Configurations.APPNAME_HEAD, new Head());
+		Environment.nameAppMaps.put(Configurations.APPNAME_TAIL, new Tail());
 		Environment.nameAppMaps.put(Configurations.APPNAME_PWD, new Pwd(Environment.currentDirectory));
 		//setup shell
 		Scanner input = new Scanner(System.in);
