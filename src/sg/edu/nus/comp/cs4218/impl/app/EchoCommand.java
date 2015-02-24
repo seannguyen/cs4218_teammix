8e6c5bed8d72e971abc33f03aed907e9b650526b
SeanNguyen <firestormkjc.1993@gmail.com>
2015-02-25 07:04:34 +0800

    resolve conflict

--- src/sg/edu/nus/comp/cs4218/impl/app/EchoCommand.java
+++ src/sg/edu/nus/comp/cs4218/impl/app/EchoCommand.java
@@ -27,14 +27,6 @@ public class EchoCommand implements Application{
 		} else {
 			StringBuilder stringBuilder = new StringBuilder();
 			for(int i = 0; i < args.length; i++) {
-<<<<<<< HEAD
-				stringBuilder.append(args[i]);
-				if (i != args.length - 1) {
-					stringBuilder.append(SPACE_SEPERATOR);
-				}
-			}
-			//stringBuilder.append(System.getProperty("line.seperator"));			
-=======
 				string = args[i];				
 				if(args[i].equals("")) {
 					return;
@@ -46,9 +38,10 @@ public class EchoCommand implements Application{
 					string = string.replaceAll("\\\\", "");
 				}
 				stringBuilder.append(string);
-				stringBuilder.append(SPACE_SEPERATOR);
+				if (i != args.length) {
+					stringBuilder.append(SPACE_SEPERATOR);
+				}
 			}					
->>>>>>> c00608f71d2be87c4f185c4b1e6d4d63b59f8b15
 			try {
 				stdout.write(stringBuilder.toString().trim().getBytes());
 			} catch (IOException e) {
