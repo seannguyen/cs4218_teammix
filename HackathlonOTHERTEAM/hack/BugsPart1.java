002b418df68d0e26a6441e709ef0043ea35bea84
weijian3030 <weijian3030@gmail.com>
2015-04-02 20:54:37 +0800

    added test cases for apps

--- HackathlonOTHERTEAM/hack/BugsPart1.java
+++ HackathlonOTHERTEAM/hack/BugsPart1.java
@@ -3,17 +3,26 @@ package hack;
 import static org.junit.Assert.assertEquals;
 import integration.Utilities;
 
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.TreeSet;
 
 import logic.ShellLogic;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import sg.edu.nus.comp.cs4218.Environment;
@@ -31,7 +40,38 @@ public class BugsPart1 {
   AppWc wc;
   static final String TEST_DIR = "testResources" + File.separator
           + "IntegrationTest";
+  private final static String FILE = "testFile1.txt";
+  private final static String FOLDER = "test_folder";
+  private final static Path FOLDER_PATH = Paths.get(FOLDER);
+  private final static Path FILE_PATH = Paths.get(FILE);
+  private final static String CONTENT = "a\nb\nc";
 
+  @BeforeClass
+  public static void setUpBeforeClass() throws IOException {		
+		FileWriter fileWriter = null;
+      BufferedWriter bufferedWriter = null;
+      try {
+      	Files.createFile(FILE_PATH);
+      	Files.createDirectory(FOLDER_PATH);
+      } catch (FileAlreadyExistsException e) {
+      	System.err.println("File already exists: " + e.getMessage());
+      }
+      
+      try {
+          File file = new File(FILE_PATH.toString());
+          file.setWritable(true);
+          fileWriter = new FileWriter(file.getAbsoluteFile());
+          bufferedWriter = new BufferedWriter(fileWriter);
+          bufferedWriter.write(CONTENT);
+          bufferedWriter.flush();
+          fileWriter.flush();
+          bufferedWriter.close();
+          fileWriter.close();
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+	}
+  
   @Before
   public void setUp() throws Exception {
 
@@ -48,7 +88,15 @@ public class BugsPart1 {
       Environment.currentDirectory = System.getProperty("user.dir");
   }
   
-  
+  @AfterClass
+	public static void tearDownAfterClass() throws IOException {
+	  try {
+		  Files.delete(FILE_PATH);
+		  Files.delete(FOLDER_PATH);
+	  } catch (IOException e) {
+		  //e.printStackTrace(); Ignore because windows cannot delete
+	  }
+	}
   
   /**
    * The bug is due to invalid handling of � symbol [|] from SHELL when passing
@@ -170,4 +218,61 @@ public class BugsPart1 {
  		testApp.run(new String[] {}, in, stdout);
  		assertEquals("File\nLines: 2\nWords: 3\nCharacters: 5\n", stdout.toString());
  	}
+     
+     /**
+      * Given one non-existing file and one existing file
+      * 
+      * The expected results should print exception msg for non-existing file
+      * but return result for existing file
+      * 
+      * Unit Level Testing
+      */
+     @Test
+ 	public void testExistingFileAndNonExistingFile() {
+ 		String[] args = {"a", FILE, FOLDER};
+ 		try {
+ 			appGrep.run(args, stdin, stdout);
+ 		} catch (AbstractApplicationException e) {
+ 			e.printStackTrace();
+ 		}
+ 		assertEquals("grep: TEST: Is a directory\ntestFile1.txt:a\n", stdout.toString());
+ 	}
+     
+     /**
+      * Given one non-existing file and one existing file
+      * 
+      * The expected results should print exception msg for non-existing file
+      * but return result for existing file
+      * 
+      * Unit Level Testing
+      */
+    @Test
+ 	public void testExistingFileAndNonExistingFile() {
+ 		String[] args = {FILE, FOLDER};
+ 		try {
+ 			appCat.run(args, stdin, stdout);
+ 		} catch (AbstractApplicationException e) {
+ 			e.printStackTrace();
+ 		}
+ 		assertEquals("cat: TEST: Is a directory\na\nb\nc", stdout.toString());
+ 	}
+    
+    /**
+     * Given one non-existing file and one existing file
+     * 
+     * The expected results should print exception msg for non-existing file
+     * but return result for existing file
+     * 
+     * Unit Level Testing
+     */
+   @Test
+	public void testExistingFileAndNonExistingFile() {
+		String[] args = {FILE, FOLDER};
+		try {
+			appWc.run(args, stdin, stdout);
+		} catch (AbstractApplicationException e) {
+			e.printStackTrace();
+		}
+		assertEquals("wc: TEST: Is a directory\nFile\nLines: 2\nWords: 3\nCharacters: 5\n", stdout.toString());
+	}
 }
